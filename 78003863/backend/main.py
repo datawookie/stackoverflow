be541d11f75557d5e092ce71de25ce3c8758aa9c
Andrew Collier <andrew@fathomdata.dev>
2024-02-16 05:46:24 +0000

    feat: Initial code for 78004328

--- 78003863/backend/main.py
+++ 78003863/backend/main.py
@@ -4,17 +4,7 @@ app = Quart(__name__)
 
 @app.route("/")
 async def hello():
-    return await render_template("index.html")
-
-@app.route("/api")
-async def json():
-    return {"hello": "world"}
-
-@app.websocket("/ws")
-async def ws():
-    while True:
-        await websocket.send("hello")
-        await websocket.send_json({"hello": "world"})
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run()
