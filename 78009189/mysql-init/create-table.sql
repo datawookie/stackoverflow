c4e76fec54877728626967b57c9e8a67893ea51d
Andrew Collier <andrew@fathomdata.dev>
2024-02-19 15:53:16 +0000

    fix: Change utf8mb3 to utf8mb4

--- 78009189/mysql-init/create-table.sql
+++ 78009189/mysql-init/create-table.sql
@@ -2,9 +2,9 @@ USE mysql;
 
 CREATE TABLE `vodovod_ulice` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `naziv` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `naziv` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `naziv` (`naziv`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 insert into vodovod_ulice (naziv) values ('Ulica Vuka Karadžića');
