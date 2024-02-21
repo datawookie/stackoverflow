a51281fa4c1a3b0920a70d3e95438a81daafa4da
Andrew Collier <andrew@fathomdata.dev>
2024-02-21 12:11:23 +0000

    feat: Add 75589560 (not working)

--- 75589560/get_dependencies.cmake
+++ 75589560/get_dependencies.cmake
@@ -5,8 +5,7 @@ FetchContent_Declare(
     igraph
     URL https://github.com/igraph/igraph/releases/download/0.10.4/igraph-0.10.4.tar.gz
     URL_HASH MD5=10a3f325425970c75a7ba8359376e208
-    FIND_PACKAGE_ARGS NAMES igraph
-    SYSTEM
+
 )
 
 FetchContent_MakeAvailable(igraph)
@@ -17,8 +16,6 @@ FetchContent_Declare(
     GEOS
     URL https://download.osgeo.org/geos/geos-3.11.1.tar.bz2
     URL_HASH MD5=5732ec96b391ecddc35bda9795b654ea
-    FIND_PACKAGE_ARGS NAMES GEOS
-    SYSTEM
 )
 
 FetchContent_MakeAvailable(GEOS)
