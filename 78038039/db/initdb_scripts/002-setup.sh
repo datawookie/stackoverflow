5d4bcae8bb45e3c832bc8f822ba58d0bfe14dd5f
Andrew Collier <andrew@fathomdata.dev>
2024-02-22 05:39:40 +0000

    fix: Working version of 78038039

--- 78038039/db/initdb_scripts/002-setup.sh
+++
@@ -1,6 +0,0 @@
-cat <<EOT >> /var/lib/postgresql/data/postgresql.conf
-shared_preload_libraries='pg_cron'
-cron.database_name='${POSTGRES_DB:-postgres}'
-EOT
-
-pg_ctl restart
--- 78038039/db/initdb_scripts/003-load-extensions.sql
+++
@@ -1,6 +0,0 @@
-CREATE EXTENSION pg_cron;
-SELECT * FROM pg_extension;
-
-CREATE SCHEMA partman;
-CREATE EXTENSION pg_partman SCHEMA partman;
-select * FROM pg_extension;
