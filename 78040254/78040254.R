e9b361bc6b73d5328f30c4ed013dfbc0f3e4f88b
Andrew Collier <andrew@fathomdata.dev>
2024-02-22 12:58:50 +0000

    feat: Add 78040254

--- 78040254/78040254.R
+++ 78040254/78040254.R
@@ -1,12 +1,10 @@
-set.seed(123)
 library(dplyr)
 library(doParallel)
 library(magrittr)
 library(tidyr)
 
-# - Create dummy data for example -|
+set.seed(123)
 
-# Large data frame - example
 df <- data.frame(matrix(nrow=150000, ncol=20))
 colnames(df) <- c('id', 'dateFrom', 'dateTo', paste0('v', seq.int(from = 1, to = 17)))
 df$id <- seq.int(from = 1, to = nrow(df))
@@ -14,7 +12,6 @@ df$dateFrom <- sample(seq(from = as.Date('2024/01/01'), to = as.Date('2024/01/31
 df$dateTo <- df$dateFrom + sample(c(0:7), nrow(df), replace=T)
 df[, grepl('v', colnames(df))] <- rbinom(nrow(df) * 17, 1, 0.05)
 
-# Data to merge
 mrg <- data.frame(matrix(nrow=120, ncol=4))
 colnames(mrg) <- c('v', 'actionDate', 'numerator', 'denominator')
 mrg$v <- c(rep('v20_n', 30), rep('v21_n', 30), rep('v22_n', 30), rep('v23_n', 30))
@@ -22,54 +19,46 @@ mrg$actionDate <- rep(seq(from = as.Date('2024/01/01'), to = as.Date('2024/01/30
 mrg$denominator <- sample(80000:120000, 120)
 mrg$numerator <- floor(mrg$denominator * runif(120, min = 0.01, max = 0.15))
 
-new <- unique(mrg$v)
-# -------------------------------- |
-
-
-# Simulation
-
 registerDoParallel(cores=8)
 
 system.time(
-  bootstrap <- foreach(x=1:32, .combine = rbind.data.frame, .packages = c('dplyr', 'magrittr', 'tidyr')) %dopar% {
-
-    df_copy <- df
-
-    for(ch in new) {
-
+  bootstrap <- foreach(n=1:32, .combine = rbind.data.frame, .packages = c('dplyr', 'magrittr', 'tidyr')) %dopar% {
+    for(ch in unique(mrg$v)) {
       sampling <- df %>%
         select(id, dateFrom, dateTo) %>%
-        cross_join(mrg %>%
-                     filter(v == ch) %>%
-                     select(actionDate, numerator, denominator)
+        cross_join(
+          mrg %>%
+            filter(v == ch) %>%
+            select(actionDate, numerator, denominator)
         ) %>%
-        filter(actionDate >= dateFrom - 3 &
-                 actionDate <= dateTo
+        filter(
+          actionDate >= dateFrom - 3 & actionDate <= dateTo
         ) %>%
         group_by(id) %>%
-        summarise(numer = sum(numerator),
-                  denom = sum(denominator),
-                  avg = numer/denom,
-                  weight = avg * (denom / 150000 * 0.2),
-                  chance = rbinom(1, 1, weight)
+        summarise(
+          numer = sum(numerator),
+          denom = sum(denominator),
+          avg = numer/denom,
+          weight = avg * (denom / 150000 * 0.2),
+          chance = rbinom(1, 1, weight)
         ) %>%
         select(id, chance) %>%
         rename(!!ch := chance) %>%
         ungroup()
 
-      df_copy <- df_copy %>%
+      df <- df %>%
         left_join(sampling, by = 'id')
-
     }
 
-    df_copy <- df_copy %>%
+    df <- df %>%
       mutate_at(vars(contains("_n")), replace_na, 0)
 
-    df_copy <- df_copy %>%
+    df <- df %>%
       group_by(across(c(-v1, -id, -dateFrom, -dateTo))) %>%
-      dplyr::summarise(Occurrences = n(), Events = sum(v1)) %>%
-      ungroup()
+      dplyr::summarise(Occurrences = n(), Events = sum(v1), .groups = "drop")
   }
+)
 
-) ## Eight cores - 40 seconds elapsed on laptop
 stopImplicitCluster()
+
+print(bootstrap)
