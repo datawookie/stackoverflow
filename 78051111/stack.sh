64f884da9625504301a87236d3284c0943ea5723
Andrew Collier <andrew@fathomdata.dev>
2024-02-24 06:39:13 +0000

    feat: Add standalone script to replicate Compose stack

---
+++ 78051111/stack.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+docker volume create vol1-db-vol
+docker volume create vol2-db-vol
+
+docker run -d --name db1 \
+    -e MYSQL_ROOT_PASSWORD=dbadmin \
+    -v vol1-db-vol:/var/lib/mysql \
+    mariadb:10.6.17-focal
+
+docker run -d --name db2 \
+    -e MYSQL_ROOT_PASSWORD=dbadmin \
+    -e MYSQL_TCP_PORT=3307 \
+    -e MYSQL_UNIX_PORT=3307 \
+    -v vol2-db-vol:/var/lib/mysql \
+    mariadb:10.5.24-focal
+
+docker run -d --name myadmin1 \
+    -e PMA_HOST=db1 \
+    -p 8080:80 \
+    phpmyadmin:latest
+
+docker run -d --name myadmin2 \
+    -e PMA_HOST=db2 \
+    -e PMA_PORT=3307 \
+    -p 8081:80 \
+    phpmyadmin:latest
