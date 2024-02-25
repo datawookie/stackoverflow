83510e5ba065b8803dffb18d20e1fab8b6568a93
Andrew Collier <andrew@fathomdata.dev>
2024-02-25 06:08:18 +0000

    fix: Remove some headers

--- 78054805/scraper.py
+++ 78054805/scraper.py
@@ -2,14 +2,9 @@ import requests
 
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Connection": "keep-alive",
-    "Upgrade-Insecure-Requests": "1",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
+    "From": "",
+    "af-ac-enc-dat": "",
+    "x-api-source": "pc",
 }
 
 params = {
@@ -23,4 +18,4 @@ params = {
 
 with requests.Session() as session:
     response = session.get("https://shopee.com.br/search", params=params, headers=headers)
-    print(response.text)
+    print(response)
