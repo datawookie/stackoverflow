b301cbdb55cc82dc88f5f49d89ee235e1bb5fc52
Andrew Collier <andrew@fathomdata.dev>
2024-03-02 05:45:54 +0000

    feat: Add 78088821

---
+++ 78088821/pages/index.js
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      <p>This is a minimal index.js file for a Next.js application.</p>
+    </div>
+  );
+}
+
+export default HomePage;
