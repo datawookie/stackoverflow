5983f95044ffa254da41dfa0acd1c9337c0de931
Andrew Collier <andrew@fathomdata.dev>
2024-03-04 06:05:49 +0000

    feat: Add 78097745

---
+++ 78097745/PuppeteerProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using PuppeteerSharp;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await new BrowserFetcher().DownloadAsync();
+        var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+        {
+            Headless = true,
+            Args = new string[] { "--no-sandbox", "--disable-setuid-sandbox" }
+        });
+
+        Console.WriteLine("Browser launched successfully.");
+
+        await browser.CloseAsync();
+    }
+}
