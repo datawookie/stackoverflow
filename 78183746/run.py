ed4fd4d8be2784c185467241703d240b3568b890
Andrew Collier <andrew@fathomdata.dev>
2024-03-19 05:26:32 +0000

    feat: Working version

--- 78183746/run.py
+++ 78183746/run.py
@@ -7,7 +7,11 @@ options.add_argument("--no-sandbox")
 options.add_argument("--headless")
 options.add_argument("--window-size=1920,1080")
 
+print("Launch Chrome.")
 driver = webdriver.Chrome(options=options)
+
+print("Get www.example.com.")
 driver.get("http://www.example.com")
 
-print(driver.page_source)
+print("Close Chrome.")
+driver.quit()
