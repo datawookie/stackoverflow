47a35b6f2de5c59b6f1165588b07505db48c9aa8
Andrew Collier <andrew@fathomdata.dev>
2024-03-28 05:19:51 +0000

    fix: Working

---
+++ 78235226/cassandra-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "🔵 Starting setup."
+
+until cqlsh -e "describe keyspaces" db_cassandra 9042 >/dev/null 2>&1; do
+  echo "🟠 Waiting for Cassandra to be ready."
+  sleep 10
+done
+
+echo "🟣 Cassandra is ready. Describe the keyspaces."
+cqlsh -e "describe keyspaces" db_cassandra 9042
+
+echo "🟢 Done!"
