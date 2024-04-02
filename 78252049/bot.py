823a383838dc5182b8a2741c68150aeca7d803ea
Andrew Collier <andrew@fathomdata.dev>
2024-04-02 05:45:04 +0100

    feat: Add 78252049

---
+++ 78252049/bot.py
@@ -0,0 +1,21 @@
+import os
+
+import telebot
+from telebot import types
+
+TOKEN = os.getenv("TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    pass
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "test")
+def handle_query(call):
+    pass
+
+
+bot.polling(non_stop=True)
