689ce4258b6ef2da6768899d82dd3595d6b2521b
Andrew Collier <andrew@fathomdata.dev>
2024-04-06 05:52:46 +0100

    fix: Update on feedback

--- 78276878/main.go
+++ 78276878/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/h2non/bimg"
 )
 
 func main() {
+	// options := bimg.Options{
+    //     Width:   800,
+    //     Height:  600,
+    //     Crop:    true,
+    //     Quality: 90,
+    // }
+	_ = bimg.VipsVersion
 	fmt.Println("Hello, World!")
 }
