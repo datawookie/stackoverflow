401ae167677a87f24b408eb336494f4f62c4891e
Andrew Collier <andrew@fathomdata.dev>
2024-04-09 06:38:28 +0100

    feat: Add 78293627 and 78295996

---
+++ 78293627/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+class Program
+{
+    public static async Task Main()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("http://playwright.dev");
+        Console.WriteLine(await page.TitleAsync());
+    }
+}
