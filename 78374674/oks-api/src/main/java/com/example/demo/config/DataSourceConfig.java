64cf52bf046a2000632d8215dd5ea206dee1d173
Andrew Collier <andrew@fathomdata.dev>
2024-04-24 05:58:14 +0100

    feat: Add 78374674 (sort of working)

---
+++ 78374674/oks-api/src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://mysql:3306/oks");
+        dataSource.setUsername("root");
+        dataSource.setPassword("roots");
+        return dataSource;
+    }
+}
