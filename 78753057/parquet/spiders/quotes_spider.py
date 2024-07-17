a76ff25edfa6976770c789b710643fd613ca4adf
Andrew Collier <andrew@fathomdata.dev>
2024-07-17 04:26:02 +0100

    feat: Add 78753057

---
+++ 78753057/parquet/spiders/quotes_spider.py
@@ -0,0 +1,27 @@
+from scrapy.spiders import CrawlSpider
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
+
+from scrapy import Item
+from scrapy import Field
+
+
+class UrlItem(Item):
+    url = Field()
+
+
+class WikiSpider(CrawlSpider):
+    name = "wiki"
+    allowed_domains = ["en.wikipedia.org"]
+    start_urls = ["https://en.wikipedia.org/wiki/Main_Page"]
+
+    seen = set()
+
+    rules = (Rule(LinkExtractor(), callback="parse_url", follow=False),)
+
+    def parse_url(self, response):
+        if response.url not in self.seen:
+            self.seen.add(response.url)
+            item = UrlItem()
+            item["url"] = response.url
+            yield item
